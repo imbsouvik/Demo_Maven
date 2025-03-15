6d96716028a7a8fcd50c0e68244184fbca8dc62d
imbsouvik <im.b.souvik@gmail.com>
2025-03-16 00:45:46 +0530

    First git commit for my project

---
+++ src/main/java/com/qa/page/Loginpage.java
@@ -0,0 +1,10 @@
+package com.qa.page;
+
+public class Loginpage {
+
+	public static void main(String[] args) {
+		System.out.println("Login Page");
+
+	}
+
+}
---
+++ src/test/java/com/qa/test/LoginTest.java
@@ -0,0 +1,10 @@
+package com.qa.test;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		System.out.println("Login Page Testing");
+
+	}
+
+}
